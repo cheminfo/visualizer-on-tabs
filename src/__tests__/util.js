3664ee323b2a1f8a6ea55e2a2b1ecb381d903b97
Daniel Kostro <kostro.d@gmail.com>
2017-05-31 15:17:08 +0200

    Write test for rewrite rules

--- src/__tests__/util.js
+++ src/__tests__/util.js
@@ -2,17 +2,28 @@
 
 import {rewriteURL} from '../util';
 
-test('Test rewriteURL', function() {
+test('Test rewriteURL dev', function() {
 
-    const config=[
-        {"reg": "^[^/]+$", "replace": "https://mydb.cheminfo.org/db/visualizer/entry/$&/view.json"},
+    const rules=[
         {"reg": "^([^/]+)\\?(.*)$", "replace": "https://couch.cheminfo.org/cheminfo-public/$1/view.json?$2"},
-        {"reg": "^[^/]+\/[^/]+$", "replace": "https://couch.cheminfo.org/$&/view.json"},
-        {"reg": "^[^/]+\/view.json.*", "replace": "https://couch.cheminfo.org/cheminfo-public/$&"},
-        {"reg": "^[^/]+\/[^/]+\/view.json.*", "replace": "https://couch.cheminfo.org/$&"}
+        {"reg": "^[^/]+$", "replace": "https://mydb.cheminfo.org/db/visualizer/entry/$&/view.json"},
+        {"reg": "^[^/]+\/view.json.*", "replace": "https://couch.cheminfo.org/cheminfo-public/$&"}
     ];
 
-    expect(rewriteURL(config, 'abc/view.json')).toEqual('xx');
+    expect(rewriteURL(rules, '15c9a2dcd55c963fdedf2c18a1471b03')).toEqual('https://mydb.cheminfo.org/db/visualizer/entry/15c9a2dcd55c963fdedf2c18a1471b03/view.json');
+    expect(rewriteURL(rules, '02235fb1ae3f9ed6437526bacfe21a98?referer=abc&rev=80-622311b3fc278ba244b6e058d8d0804f')).toEqual('https://couch.cheminfo.org/cheminfo-public/02235fb1ae3f9ed6437526bacfe21a98/view.json?referer=abc&rev=80-622311b3fc278ba244b6e058d8d0804f');
+    expect(rewriteURL(rules, '02235fb1ae3f9ed6437526bacfe21a98/view.json?referer=abc&rev=80-622311b3fc278ba244b6e058d8d0804f')).toEqual('https://couch.cheminfo.org/cheminfo-public/02235fb1ae3f9ed6437526bacfe21a98/view.json?referer=abc&rev=80-622311b3fc278ba244b6e058d8d0804f');
+});
 
+test('Test rewriteURL prod', function () {
+   const rules = [
+       {"reg": "^([^/]+)\\?(.*)$", "replace": "https://couch.cheminfo.org/cheminfo-public/$1/view.json?$2"},
+       {"reg": "^[^/]+$", "replace": "https://couch.cheminfo.org/cheminfo-public/$&/view.json"},
+       {"reg": "^[^/]+\/view.json.*", "replace": "https://couch.cheminfo.org/cheminfo-public/$&"}
+   ];
+
+    expect(rewriteURL(rules, '15c9a2dcd55c963fdedf2c18a1471b03')).toEqual('https://couch.cheminfo.org/cheminfo-public/15c9a2dcd55c963fdedf2c18a1471b03/view.json');
+    expect(rewriteURL(rules, '02235fb1ae3f9ed6437526bacfe21a98?referer=abc&rev=80-622311b3fc278ba244b6e058d8d0804f')).toEqual('https://couch.cheminfo.org/cheminfo-public/02235fb1ae3f9ed6437526bacfe21a98/view.json?referer=abc&rev=80-622311b3fc278ba244b6e058d8d0804f');
+    expect(rewriteURL(rules, '02235fb1ae3f9ed6437526bacfe21a98/view.json?referer=abc&rev=80-622311b3fc278ba244b6e058d8d0804f')).toEqual('https://couch.cheminfo.org/cheminfo-public/02235fb1ae3f9ed6437526bacfe21a98/view.json?referer=abc&rev=80-622311b3fc278ba244b6e058d8d0804f');
 
 });
