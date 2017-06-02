461c13dfd0ecc34d2944685a02c3ce08bb6211d9
Daniel Kostro <kostro.d@gmail.com>
2017-06-02 11:21:23 +0200

    multi rewrite rules

--- src/__tests__/util.js
+++ src/__tests__/util.js
@@ -17,7 +17,15 @@ const dockerDevRules = [
     {"reg": "^public/([a-z0-9]+)$", "replace": "https://couch.cheminfo.org/cheminfo-public/$1/view.json"},
     {"reg": "^public/([a-z0-9]+)\\?(.*)$", "replace": "https://couch.cheminfo.org/cheminfo-public/$1/view.json?$2"},
     {"reg": "^private/([a-z0-9]+)$", "replace": "../roc/db/visualizer/entry/$1/view.json"},
-    {"reg": "^private/([a-z0-9]+)\\?(.*)$", "replace": "../roc/db/visualizer/entry/$1/view.json"},
+    [
+        {"reg": "^private/([a-z0-9]+)\\?(.*)$", "replace": "../roc/db/visualizer/entry/$1/view.json?$2"},
+        {"reg": "rev=[a-z0-9\\-]+", "replace": ""},
+        {"reg": "&&", "replace": "&", "optionalMatch": true},
+        {"reg": "\\?&", "replace": "?", "optionalMatch": true},
+        {"reg": "\\?$", "replace": "", "optionalMatch": true},
+        {"reg": "\\&$", "replace": "", "optionalMatch": true},
+
+    ],
     {"reg": "^[a-z0-9]+$", "replace": "https://couch.cheminfo.org/cheminfo-public/$&/view.json"},
     {"reg": "^([a-z0-9]+)\\?(.*)$", "replace": "https://couch.cheminfo.org/cheminfo-public/$1/view.json?$2"},
     {"reg": "^[^/]+\/view.json.*", "replace": "https://couch.cheminfo.org/cheminfo-public/$&"}
@@ -41,8 +49,6 @@ describe('dev rewrite rules', function () {
         '15c9a2dcd55c963fdedf2c18a1471b03',
         'https://mydb.cheminfo.org/db/visualizer/entry/15c9a2dcd55c963fdedf2c18a1471b03/view.json'
     );
-
-
 
     assertRewrite(
         'should rewrite uuid with rev by ignoring rev',
@@ -117,7 +123,7 @@ describe('docker prod rewrite rules', function () {
     );
 
     assertRewrite(
-        'should rewrite explicit private uuid without the query string',
+        'should rewrite explicit private uuid with the query string',
         'private/15c9a2dcd55c963fdedf2c18a1471b03?rev=161-13da947c771e6847466bc8f0cd43f9ae',
         '../roc/db/visualizer/entry/15c9a2dcd55c963fdedf2c18a1471b03/view.json?rev=161-13da947c771e6847466bc8f0cd43f9ae'
     );
@@ -180,9 +186,21 @@ describe('docker dev rewrite rules', function () {
     );
 
     assertRewrite(
-        'should rewrite explicit private uuid with query string',
+        'should rewrite explicit private uuid without the revision',
         'private/15c9a2dcd55c963fdedf2c18a1471b03?rev=161-13da947c771e6847466bc8f0cd43f9ae',
         '../roc/db/visualizer/entry/15c9a2dcd55c963fdedf2c18a1471b03/view.json'
+    );
+
+    assertRewrite(
+        'should rewrite explicit private uuid without the revision but with the referer (1)',
+        'private/15c9a2dcd55c963fdedf2c18a1471b03?referer=abcd.com&rev=161-13da947c771e6847466bc8f0cd43f9ae',
+        '../roc/db/visualizer/entry/15c9a2dcd55c963fdedf2c18a1471b03/view.json?referer=abcd.com'
+    );
+
+    assertRewrite(
+        'should rewrite explicit private uuid without the revision but with the referer (2)',
+        'private/15c9a2dcd55c963fdedf2c18a1471b03?rev=161-13da947c771e6847466bc8f0cd43f9ae&referer=abcd.com',
+        '../roc/db/visualizer/entry/15c9a2dcd55c963fdedf2c18a1471b03/view.json?referer=abcd.com'
     );
 
     assertRewrite(
